2a3c0803c52198c9f9c17cc4d6295e15bf0ca686
Arka <arka.spectacular@gmail.com>
2021-07-23 18:31:01 +0530

    [Arka]Add:6 assignments added

--- Logical_Programs/Program.cs
+++ Logical_Programs/Program.cs
@@ -14,12 +14,7 @@ namespace Logical_Programs
             Console.WriteLine("Press 3 for Prime or not!");
             Console.WriteLine("Press 4 for Reversing a Number");
             Console.WriteLine("Press 5 for Coupon Numbers!");
-
-            Console.WriteLine("Press 6 for !");
-            Console.WriteLine("Press 7 for Swap_Numbers!");
-            Console.WriteLine("Press 8 for Even or Odd!");
-            Console.WriteLine("Press 9 for Vowel_Or_Constant!");
-            Console.WriteLine("Press 10 for Largest_Among_Three_Numbers!");
+            Console.WriteLine("Press 6 for Stop_Watch!");
             Console.WriteLine("----------------------------------------------------------------------------------------");
             choice = Convert.ToInt32(Console.ReadLine());
             switch (choice)
@@ -53,35 +48,15 @@ namespace Logical_Programs
                      Coupon_Number.coupon_Number();
                      break;
 
-                     /* case 6:
-                          Console.WriteLine("Quotient and Remainder");
-                          Quotient_and_Remainder quo = new Quotient_and_Remainder();
-                          Quotient_and_Remainder.Quotient_and_Remainders();
+                case 6:
+                     Console.WriteLine("Stop_Watch");
+                     Stop_Watch t = new Stop_Watch();
+                    Stop_Watch.stop_Watch();
                           break;
-                      case 7:
-                          Console.WriteLine("Swap Two Numbers");
-                          Swap_Number swp = new Swap_Number();
-                          Swap_Number.swap();
-                          break;
-
-                      case 8:
-                          Console.WriteLine("Even Or Odd");
-                          Even_odd e = new Even_odd();
-                          Even_odd.check();
-                          break;
-                      case 9:
-                          Console.WriteLine("Vowel Or Consonant");
-                          Vowel_Consonant v = new Vowel_Consonant();
-                          Vowel_Consonant.check();
-                          break;
-                      case 10:
-                          Console.WriteLine("Largest_Among_Three_Numbers");
-                          Largest_Three_Numbers l = new Largest_Three_Numbers();
-                          Largest_Three_Numbers.check();
-                          break;
+                    
                       default:
                           Console.WriteLine("Wrong Input!");
-                          break;  */
+                          break;  
             }
             }
         }
---
+++ Logical_Programs/Stop_Watch.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+public class Stop_Watch
+{
+    public static void stop_Watch()
+    {
+        String check = "";
+       
+
+        Boolean counter;
+        Stopwatch stopwatch = new Stopwatch();
+        Console.WriteLine("Press S to begin the stopwatch");
+        Console.WriteLine("Press T to stop the stopwatch");
+        check = Console.ReadLine();
+        if (check== "S")
+        {
+            stopwatch.Start();
+            check = Console.ReadLine();
+            
+                while(check!="T")
+                {
+                    Thread.Sleep(1);
+                } 
+        }
+            stopwatch.Stop();
+    
+        Console.WriteLine("Time elapsed: {0}", stopwatch.Elapsed);
+    }
+}
